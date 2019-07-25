2fc873945a81a84963c362e1f21bb1db8702ba54
Anil-KSharma <53290847+Anil-KSharma@users.noreply.github.com>
2019-07-25 12:17:54 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,11 +11,12 @@ namespace Inagram
         static void Main(string[] args)
         {
             Program p = new Program();
-            p.Inagram("abcd","abcd");
+            p.Anagram("abcd","abcd");
             int[] arr = { 10, 1, 8, 4, 5, 6 };
             p.ArrayMax(arr);
             Console.ReadKey();
         }
+	// Retrieving max value from an array.
         public void ArrayMax( int[] value)
         {
             int mv=int.MinValue;
@@ -28,7 +29,8 @@ namespace Inagram
             }
             Console.Write(mv);
         }
-        public void Inagram(string A, string B)
+	//Anagram implementation.
+        public void Anagram(string A, string B)
         {
             string sb = "Anagram true !";
             int acount=0,bcount=0;
